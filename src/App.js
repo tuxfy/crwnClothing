f8584c386238cca802e531b6be801cfc8bffcf77
tuxfy <social.tux@posteo.de>
2020-05-26 09:29:37 +0200

    seperated homepage components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from './hompepage.component'
+import HomePage from './pages/homepage/hompepage.component'
 
 function App() {
   return (
