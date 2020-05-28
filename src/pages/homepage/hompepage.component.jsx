1b11d779554717191890cc66e537e9f41d10d817
tuxfy <social.tux@posteo.de>
2020-05-28 07:49:53 +0200

    added basic router functionality

--- src/pages/homepage/hompepage.component.jsx
+++ src/pages/homepage/hompepage.component.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 import './homepage.styles.scss';
 import Directory from '../../components/directory/directory.component';
 
-const HomePage = () => {
-    return (
-        <div className="homepage">
-            <Directory  />
-        </div>
-    );
+const HomePage = () => {   
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
 };
 
 
