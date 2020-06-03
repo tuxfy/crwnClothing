591875e52b4116a67e5fdb86cb7c795a9dca7140
tuxfy <social.tux@posteo.de>
2020-06-03 07:57:36 +0200

    added signin + input + button component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Header from './components/header/header.component';
 import HomePage from './pages/homepage/hompepage.component';
 import Shop from './pages/shop/shop.component';
+import SignInAndSigOut from './pages/sigin-in-and-sign-up/sigin-in-and-sign-up';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={Shop} />
+        <Route path='/signin' component={SignInAndSigOut} />
       </Switch>
     </div>
   );
---
+++ src/pages/sigin-in-and-sign-up/sigin-in-and-sign-up.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import SigIn from '../../components/sign-in/sign-in.component';
+import SignIn from '../../components/sign-in/sign-in.component';
+
+const SignInAndSignUp = () => {
+  return (
+    <div className="sigin-in-and-sign-up">
+      <SignIn />
+    </div>
+  );
+};
+
+export default SignInAndSignUp;
