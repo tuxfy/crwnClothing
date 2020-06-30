8c432d97f28664e14e4b9f350a497f9f887f6982
tuxfy <social.tux@posteo.de>
2020-06-30 15:58:51 +0200

    added cart selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from "react-redux";
 
 import CustomButton from '../custom-button/custom-button.component';
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 
 import './cart-dropdown.styles.scss';
 import CartItem from '../cart-item/cart-item.component';
@@ -21,8 +22,8 @@ const CartDropdown = ({ cartItems }) => {
   );
 };
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state)
 })
 
 export default connect(
